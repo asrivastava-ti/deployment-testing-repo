07af95f160493b22faef9824730a931d18e9442c
Ayush Srivastava <ayush.srivastava02@telusdigital.com>
2025-08-18 20:22:18 +0530

    fixed

--- src/lambda1/app.py
+++ src/lambda1/app.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda 1 good!'
+        'body': 'Hello from Lambda 1 good everything working!'
     }
