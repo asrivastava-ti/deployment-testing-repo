e14ea6e81d6029ed1d07d7918145994b495a5b9c
Ayush Srivastava <ayush.srivastava02@telusdigital.com>
2025-09-12 23:58:53 +0530

    fixed

--- src/functions/order-service/lambda_function.py
+++ src/functions/order-service/lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda order-service good everything Emergency code change shljsdhvclsjdhclskj'
+        'body': 'Hello from Lambda order-service good everything Emergency code change shljsdhvclsjdhclskj...Added after resolution'
     }
