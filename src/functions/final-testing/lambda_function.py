60ac1bc7f66ece9ebe2c843fd118d4d8fca417e4
Ayush Srivastava <ayush.srivastava02@telusdigital.com>
2025-09-12 13:37:23 +0530

    fixed

--- src/functions/final-testing/lambda_function.py
+++ src/functions/final-testing/lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda final-testing good everything working!'
+        'body': 'Hello from Lambda final-testing good everything working! Executing TC1'
     }
