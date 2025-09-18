f4cca021d3e80cc9f62ae96a8da324bbc23c4084
Ayush Srivastava <ayush.srivastava02@telusdigital.com>
2025-09-18 21:27:49 +0530

    fixed

--- src/functions/order-service/lambda_function.py
+++
@@ -1,5 +0,0 @@
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda order-service good everything Emergency code change shljsdhvclsjdhclskj...Added after resolution'
-    }
--- src/functions/testFunction/lambda_function.py
+++
@@ -1,5 +0,0 @@
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda testFunction good everything working!! Changed from github and dfrit introduced....added so many things!  !!'
-    }
